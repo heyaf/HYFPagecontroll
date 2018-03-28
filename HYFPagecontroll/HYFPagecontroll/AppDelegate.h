e93b79b7805ba4e462b58f0c8a5ab41c912b19a6
熊本熊 <apple@hongdingdeMac-mini.local>
2018-03-28 16:23:25 +0800

    first commit

---
+++ HYFPagecontroll/HYFPagecontroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYFPagecontroll
+//
+//  Created by 弘鼎 on 2018/3/28.
+//  Copyright © 2018年 贺亚飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYFPagecontroll/HYFPagecontroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYFPagecontroll
+//
+//  Created by 弘鼎 on 2018/3/28.
+//  Copyright © 2018年 贺亚飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
